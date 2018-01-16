24441ed05d61b876e38de87f5dc27592668eaf73
Rajeev Jaggavarapu <rajeev.jaggavarapu@srijan.net>
2018-01-16 14:20:26 +0530

    updated index page

--- myapp/index.php
+++ myapp/index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Example Title</title>
+        <title>Hello Reddy</title>
     </head>
     <body>
+    <h1>
+    How are you?????
+    </h1>
         <p>
             Hello. Today is <?= date('l \t\h\e jS') ?>.
         </p>
